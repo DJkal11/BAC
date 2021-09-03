5afa35e76ed84872b01523cec106ce06489b7cff
DJkal11 <73737598+DJkal11@users.noreply.github.com>
2021-09-03 11:36:14 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import InputArea from "./InputArea";
+import WarningIcon from "@material-ui/icons/Warning";
+
+function App() {
+  const [calBAC, setBAC] = useState(0);
+  const [condition, setCondition] = useState(false);
+
+  function calcItem(inputInfo) {
+    console.log(inputInfo);
+    const floor = Math.floor;
+    const Drinks = floor(parseFloat(inputInfo.numDrinks));
+    const Weight = floor(parseFloat(inputInfo.bodyWeight)) * 1000;
+    const genderNum = parseFloat(inputInfo.gender);
+    console.log(genderNum);
+    console.log(Drinks);
+    console.log(Weight);
+
+    const calResult = ((12 * Drinks) / (Weight * genderNum)) * 100;
+    const roundedResult = Math.round(calResult * 100) / 100;
+    console.log(roundedResult);
+    setBAC(roundedResult);
+
+    setCondition(() => {
+      if (roundedResult >= 0.08) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    document.getElementById("display").style.display = "block";
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <img
+          className="Beerimg"
+          src="https://png.pngitem.com/pimgs/s/185-1852551_gfycat-gif-brewery-beer-stein-beer-gif-beer.png"
+          alt="Beer"
+        />
+        <h1>Blood Alcohol Content</h1>
+      </div>
+      <InputArea onCalc={calcItem} />
+      <div id="display" style={{ display: "none" }}>
+        <h1>Your BAC level is {calBAC}</h1>
+        <h1>
+          {condition
+            ? "You are intoxicated and not permitted to drive!"
+            : "You are permitted to drive."}
+        </h1>
+      </div>
+
+      <div className="warning">
+        <h2>
+          <WarningIcon />
+          Warning
+        </h2>
+        <ul>
+          <li>
+            <b>The Widmark formula is only a rough indicator of BAC.</b>
+          </li>
+
+          <li>
+            <b>
+              It is never okay to drive after drinking regardless of what you
+              may calculate as your BAC using this formula.
+            </b>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
